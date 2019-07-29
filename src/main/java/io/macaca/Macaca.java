328ed41348a8025d5669f01e9ecfce249b94c6e6
Mayowa Aj <sagaraaj69@gmail.com>
2019-07-29 09:25:55 +0100

    main class

---
+++ src/main/java/io/macaca/Macaca.java
@@ -0,0 +1,13 @@
+package io.macaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Macaca {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Macaca.class, args);
+    }
+
+}
