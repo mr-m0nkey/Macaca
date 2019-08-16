d2330d8d2ea4694a71370acaf8cd323bc73b8506
Mayowa Aj <sagaraaj69@gmail.com>
2019-08-16 09:21:34 +0100

    can register itself on the eureka server

--- src/main/java/io/macaca/Macaca.java
+++ src/main/java/io/macaca/Macaca.java
@@ -2,12 +2,17 @@ package io.macaca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 public class Macaca {
 
     public static void main(String[] args) {
         SpringApplication.run(Macaca.class, args);
     }
+
 
 }
